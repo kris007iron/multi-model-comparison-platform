e0adfab6526a2b4d71fb007a5059cd893b095c5d
kris007iron <kris007.iron@gmail.com>
2023-12-11 13:25:18 +0100

    Update .gitignore file to exclude Python, Docker, IDE, and compiled files

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from time import time
+
+app = FastAPI()
+text_generator = pipeline("text-generation")
+
+@app.get("/compare")
+def compare_models(query: str):
+    start_time = time()
+    local_model_response = text_generator(query, max_length=30)[0]['generated_text']
+    end_time_local = time() - start_time
+
+    # Add logic for external API (OpenAI, LlamaAPI) comparison here
+
+    return {
+        "local_model_response": local_model_response,
+        "external_model_response": "External API Response Placeholder",
+        "response_times": {
+            "local_model": end_time_local,
+            "external_model": "External API Response Time Placeholder",
+        },
+    }
