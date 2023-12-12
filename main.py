e4208f815c5cb4f5d53a78d0f0edf1e1d8536b38
kris007iron <kris007.iron@gmail.com>
2023-12-12 09:51:12 +0100

    Update text generator model and add Hugging Face API integration

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
+import requests
 from transformers import pipeline, AutoModelForCausalLM
 from time import time
 from llamaapi import LlamaAPI
 
+
 app = FastAPI()
-text_generator = pipeline("text-generation") #this too , model="openchat/openchat_3.5"
+text_generator = pipeline("sentiment-analysis") #this too , model="openchat/openchat_3.5"
+#remeote hugging face impl
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+headers = {"Authorization": "Bearer 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"}
+def query_h(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 #AutoModelForCausalLM.from_pretrained("openchat/openchat_3.5") for local model full functionality
 lApiToken = "LL-LalmintU3wY0xybcJHVuGrn7RF65Uhc89YYpsXjk9onnAagUtzv7Dr0eQmXQz8eq"
 llama = LlamaAPI(lApiToken)
 
+
 @app.get("/compare")
 def compare_models(query: str):
     start_time = time()
-    local_model_response = text_generator(query, max_length=30)[0]['generated_text']
+    local_model_response = text_generator(query)#, max_length=30)[0]['generated_text']
     end_time_local = time() - start_time
     api_request_json = {
     "messages": [
@@ -39,12 +49,16 @@ def compare_models(query: str):
     response = llama.run(api_request_json)
     end_time_external = time() - start_time2
     # Add logic for external API (OpenAI, LlamaAPI) comparison here
-
+    start_time3 = time()
+    responseh = query_h({"inputs": query})
+    end_time_hugging = time() - start_time3
     return {
         "local_model_response": local_model_response,
         "external_model_response": response.json(),
+        "external_huggingface_response": responseh,
         "response_times": {
             "local_model": end_time_local,
             "external_model": end_time_external,
+            "external_huggingface": end_time_hugging,
         },
     }
